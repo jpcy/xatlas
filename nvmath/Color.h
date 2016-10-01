5a6948dbe09bb3a85ea1ba3ee58622372dd294f0
Jonathan Young <young.jpc@gmail.com>
2016-10-01 16:28:20 +1000

    Remove unused Color classes.

--- nvmath/Color.h
+++ nvmath/Color.h
@@ -8,43 +8,6 @@
 
 namespace nv
 {
-
-    /// 64 bit color stored as BGRA.
-    class NVMATH_CLASS Color64 
-    {
-    public:
-        Color64() { }
-        Color64(const Color64 & c) : u(c.u) { }
-        Color64(uint16 R, uint16 G, uint16 B, uint16 A) { setRGBA(R, G, B, A); }
-        explicit Color64(uint64 U) : u(U) { }
-
-        void setRGBA(uint16 R, uint16 G, uint16 B, uint16 A)
-        {
-            r = R;
-            g = G;
-            b = B;
-            a = A;
-        }
-
-        operator uint64 () const {
-            return u;
-        }
-
-        union {
-            struct {
-#if NV_LITTLE_ENDIAN
-                uint16 r, a, b, g;
-#else
-                uint16 a: 16;
-                uint16 r: 16;
-                uint16 g: 16;
-                uint16 b: 16;
-#endif
-            };
-            uint64 u;
-        };
-    };
-
     /// 32 bit color stored as BGRA.
     class NVMATH_CLASS Color32
     {
@@ -91,57 +54,6 @@ namespace nv
             };
             uint8 component[4];
             uint32 u;
-        };
-    };
-
-
-    /// 16 bit 565 BGR color.
-    class NVMATH_CLASS Color16
-    {
-    public:
-        Color16() { }
-        Color16(const Color16 & c) : u(c.u) { }
-        explicit Color16(uint16 U) : u(U) { }
-
-        union {
-            struct {
-#if NV_LITTLE_ENDIAN
-                uint16 b : 5;
-                uint16 g : 6;
-                uint16 r : 5;
-#else
-                uint16 r : 5;
-                uint16 g : 6;
-                uint16 b : 5;
-#endif
-            };
-            uint16 u;
-        };
-    };
-
-    /// 16 bit 4444 BGRA color.
-    class NVMATH_CLASS Color16_4444
-    {
-    public:
-        Color16_4444() { }
-        Color16_4444(const Color16_4444 & c) : u(c.u) { }
-        explicit Color16_4444(uint16 U) : u(U) { }
-
-        union {
-            struct {
-#if NV_LITTLE_ENDIAN
-                uint16 b : 4;
-                uint16 g : 4;
-                uint16 r : 4;
-                uint16 a : 4;
-#else
-                uint16 a : 4;
-                uint16 r : 4;
-                uint16 g : 4;
-                uint16 b : 4;
-#endif
-            };
-            uint16 u;
         };
     };
 
