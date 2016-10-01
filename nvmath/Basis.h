06eba2a2891436373d5f8f9e70f8220242a3d7c3
Jonathan Young <young.jpc@gmail.com>
2016-10-01 23:11:39 +1000

    Removed unused Basic code.

--- nvmath/Basis.h
+++ nvmath/Basis.h
@@ -20,57 +20,7 @@ namespace nv
         /// Create a null basis.
         Basis() : tangent(0, 0, 0), bitangent(0, 0, 0), normal(0, 0, 0) {}
 
-        /// Create a basis given three vectors.
-        Basis(Vector3::Arg n, Vector3::Arg t, Vector3::Arg b) : tangent(t), bitangent(b), normal(n) {}
-
-        /// Create a basis with the given tangent vectors and the handness.
-        Basis(Vector3::Arg n, Vector3::Arg t, float sign)
-        {
-            build(n, t, sign);
-        }
-
-        void normalize(float epsilon = NV_EPSILON);
-        void orthonormalize(float epsilon = NV_EPSILON);
-        void robustOrthonormalize(float epsilon = NV_EPSILON);
         void buildFrameForDirection(Vector3::Arg d, float angle = 0);
-
-        /// Calculate the determinant [ F G N ] to obtain the handness of the basis. 
-        float handness() const
-        {
-            return determinant() > 0.0f ? 1.0f : -1.0f;
-        }
-
-        /// Build a basis from 2 vectors and a handness flag.
-        void build(Vector3::Arg n, Vector3::Arg t, float sign)
-        {
-            normal = n;
-            tangent = t;
-            bitangent = sign * cross(t, n);
-        }
-
-        /// Compute the determinant of this basis.
-        float determinant() const
-        {
-            return 
-                tangent.x * bitangent.y * normal.z - tangent.z * bitangent.y * normal.x +
-                tangent.y * bitangent.z * normal.x - tangent.y * bitangent.x * normal.z + 
-                tangent.z * bitangent.x * normal.y - tangent.x * bitangent.z * normal.y;
-        }
-
-        bool isValid() const;
-
-        // Get transform matrix for this basis.
-        Matrix matrix() const;
-
-        // Transform by this basis. (From this basis to object space).
-        Vector3 transform(Vector3::Arg v) const;
-
-        // Transform by the transpose. (From object space to this basis).
-        Vector3 transformT(Vector3::Arg v);
-
-        // Transform by the inverse. (From object space to this basis).
-        Vector3 transformI(Vector3::Arg v) const;
-
 
         Vector3 tangent;
         Vector3 bitangent;
