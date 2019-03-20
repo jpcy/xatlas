4e7c452fd96fa57bb6ba8fe674546e4f16f1483f
Jonathan Young <young.jpc@gmail.com>
2019-03-20 21:45:22 +1000

    Viewer: use bgfx callback.

--- extra/viewer.cpp
+++ extra/viewer.cpp
@@ -2039,6 +2039,33 @@ static void bakeClear()
 	s_options.shadeMode = s_atlas.status.get() == AtlasStatus::Ready ? ShadeMode::Charts : ShadeMode::Flat;
 }
 
+struct BgfxCallback : public bgfx::CallbackI
+{
+	virtual ~BgfxCallback() {}
+
+	void fatal(const char* /*_filePath*/, uint16_t /*_line*/, bgfx::Fatal::Enum /*_code*/, const char* _str) override
+	{
+		fprintf(stderr, "%s\n", _str);
+		exit(EXIT_FAILURE);
+	}
+
+	void traceVargs(const char* /*_filePath*/, uint16_t /*_line*/, const char* _format, va_list _argList) override
+	{
+		bx::debugPrintfVargs(_format, _argList);
+	}
+
+	void profilerBegin(const char*, uint32_t, const char*, uint16_t) override {}
+	void profilerBeginLiteral(const char*, uint32_t, const char*, uint16_t) override {}
+	void profilerEnd() override {}
+	uint32_t cacheReadSize(uint64_t) override { return 0; }
+	bool cacheRead(uint64_t, void*, uint32_t) override { return false; }
+	void cacheWrite(uint64_t, const void*, uint32_t) override {}
+	void screenShot(const char*, uint32_t, uint32_t, uint32_t, const void*, uint32_t, bool) override {}
+	void captureBegin(uint32_t, uint32_t, uint32_t, bgfx::TextureFormat::Enum, bool) override {}
+	void captureEnd() override {}
+	void captureFrame(const void*, uint32_t) override {}
+};
+
 int main(int argc, char **argv)
 {
 	bx::CommandLine commandLine(argc, argv);
@@ -2051,7 +2078,9 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	glfwMaximizeWindow(s_window);
 	bgfx::renderFrame();
+	BgfxCallback bgfxCallback;
 	bgfx::Init init;
+	init.callback = &bgfxCallback;
 	if (commandLine.hasArg("gl"))
 		init.type = bgfx::RendererType::OpenGL;
 #if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
