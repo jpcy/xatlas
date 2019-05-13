9dd33af33e1d9a7356357fd9fdbce4327cd7ed45
Jonathan Young <young.jpc@gmail.com>
2019-05-13 20:58:04 +1000

    Viewer: cleanup rasterizer.

--- extra/viewer_bake.cpp
+++ extra/viewer_bake.cpp
@@ -259,68 +259,19 @@ static bool bakeIsWorkerThreadCancelled()
 	return s_bake.cancelWorker;
 }
 
-typedef int lm_bool;
-#define LM_FALSE 0
-#define LM_TRUE  1
-
-#if defined(_MSC_VER) && !defined(__cplusplus) // TODO: specific versions only?
-#define inline __inline
-#endif
-
-#if defined(_MSC_VER) && (_MSC_VER <= 1700)
-static inline lm_bool lm_finite(float a) { return _finite(a); }
-#else
-static inline lm_bool lm_finite(float a) { return isfinite(a); }
-#endif
-
-static inline int      lm_mini      (int     a, int     b) { return a < b ? a : b; }
-static inline int      lm_maxi      (int     a, int     b) { return a > b ? a : b; }
-static inline int      lm_absi      (int     a           ) { return a < 0 ? -a : a; }
-static inline float    lm_minf      (float   a, float   b) { return a < b ? a : b; }
-static inline float    lm_maxf      (float   a, float   b) { return a > b ? a : b; }
-static inline float    lm_absf      (float   a           ) { return a < 0.0f ? -a : a; }
-
-typedef struct lm_ivec2 { int x, y; } lm_ivec2;
-static inline lm_ivec2 lm_i2        (int     x, int     y) { lm_ivec2 v = { x, y }; return v; }
-
 typedef struct lm_vec2 { float x, y; } lm_vec2;
-static inline lm_vec2  lm_v2i       (int     x, int     y) { lm_vec2 v = { (float)x, (float)y }; return v; }
-static inline lm_vec2  lm_v2        (float   x, float   y) { lm_vec2 v = { x, y }; return v; }
-static inline lm_vec2  lm_negate2   (lm_vec2 a           ) { return lm_v2(-a.x, -a.y); }
-static inline lm_vec2  lm_add2      (lm_vec2 a, lm_vec2 b) { return lm_v2(a.x + b.x, a.y + b.y); }
-static inline lm_vec2  lm_sub2      (lm_vec2 a, lm_vec2 b) { return lm_v2(a.x - b.x, a.y - b.y); }
-static inline lm_vec2  lm_mul2      (lm_vec2 a, lm_vec2 b) { return lm_v2(a.x * b.x, a.y * b.y); }
-static inline lm_vec2  lm_scale2    (lm_vec2 a, float   b) { return lm_v2(a.x * b, a.y * b); }
-static inline lm_vec2  lm_div2      (lm_vec2 a, float   b) { return lm_scale2(a, 1.0f / b); }
-static inline lm_vec2  lm_min2      (lm_vec2 a, lm_vec2 b) { return lm_v2(lm_minf(a.x, b.x), lm_minf(a.y, b.y)); }
-static inline lm_vec2  lm_max2      (lm_vec2 a, lm_vec2 b) { return lm_v2(lm_maxf(a.x, b.x), lm_maxf(a.y, b.y)); }
-static inline lm_vec2  lm_floor2    (lm_vec2 a           ) { return lm_v2(floorf(a.x), floorf(a.y)); }
-static inline lm_vec2  lm_ceil2     (lm_vec2 a           ) { return lm_v2(ceilf (a.x), ceilf (a.y)); }
-static inline float    lm_dot2      (lm_vec2 a, lm_vec2 b) { return a.x * b.x + a.y * b.y; }
-static inline float    lm_cross2    (lm_vec2 a, lm_vec2 b) { return a.x * b.y - a.y * b.x; } // pseudo cross product
-static inline float    lm_length2sq (lm_vec2 a           ) { return a.x * a.x + a.y * a.y; }
-static inline float    lm_length2   (lm_vec2 a           ) { return sqrtf(lm_length2sq(a)); }
-static inline lm_vec2  lm_normalize2(lm_vec2 a           ) { return lm_div2(a, lm_length2(a)); }
-static inline lm_bool  lm_finite2   (lm_vec2 a           ) { return lm_finite(a.x) && lm_finite(a.y); }
-
-typedef struct lm_vec3 { float x, y, z; } lm_vec3;
-static inline lm_vec3  lm_v3        (float   x, float   y, float   z) { lm_vec3 v = { x, y, z }; return v; }
-static inline lm_vec3  lm_negate3   (lm_vec3 a           ) { return lm_v3(-a.x, -a.y, -a.z); }
-static inline lm_vec3  lm_add3      (lm_vec3 a, lm_vec3 b) { return lm_v3(a.x + b.x, a.y + b.y, a.z + b.z); }
-static inline lm_vec3  lm_sub3      (lm_vec3 a, lm_vec3 b) { return lm_v3(a.x - b.x, a.y - b.y, a.z - b.z); }
-static inline lm_vec3  lm_mul3      (lm_vec3 a, lm_vec3 b) { return lm_v3(a.x * b.x, a.y * b.y, a.z * b.z); }
-static inline lm_vec3  lm_scale3    (lm_vec3 a, float   b) { return lm_v3(a.x * b, a.y * b, a.z * b); }
-static inline lm_vec3  lm_div3      (lm_vec3 a, float   b) { return lm_scale3(a, 1.0f / b); }
-static inline lm_vec3  lm_min3      (lm_vec3 a, lm_vec3 b) { return lm_v3(lm_minf(a.x, b.x), lm_minf(a.y, b.y), lm_minf(a.z, b.z)); }
-static inline lm_vec3  lm_max3      (lm_vec3 a, lm_vec3 b) { return lm_v3(lm_maxf(a.x, b.x), lm_maxf(a.y, b.y), lm_maxf(a.z, b.z)); }
-static inline lm_vec3  lm_floor3    (lm_vec3 a           ) { return lm_v3(floorf(a.x), floorf(a.y), floorf(a.z)); }
-static inline lm_vec3  lm_ceil3     (lm_vec3 a           ) { return lm_v3(ceilf (a.x), ceilf (a.y), ceilf (a.z)); }
-static inline float    lm_dot3      (lm_vec3 a, lm_vec3 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
-static inline lm_vec3  lm_cross3    (lm_vec3 a, lm_vec3 b) { return lm_v3(a.y * b.z - b.y * a.z, a.z * b.x - b.z * a.x, a.x * b.y - b.x * a.y); }
-static inline float    lm_length3sq (lm_vec3 a           ) { return a.x * a.x + a.y * a.y + a.z * a.z; }
-static inline float    lm_length3   (lm_vec3 a           ) { return sqrtf(lm_length3sq(a)); }
-static inline lm_vec3  lm_normalize3(lm_vec3 a           ) { return lm_div3(a, lm_length3(a)); }
-static inline lm_bool  lm_finite3   (lm_vec3 a           ) { return lm_finite(a.x) && lm_finite(a.y) && lm_finite(a.z); }
+static lm_vec2  lm_v2i       (int     x, int     y) { lm_vec2 v = { (float)x, (float)y }; return v; }
+static lm_vec2  lm_v2        (float   x, float   y) { lm_vec2 v = { x, y }; return v; }
+static lm_vec2  lm_add2      (lm_vec2 a, lm_vec2 b) { return lm_v2(a.x + b.x, a.y + b.y); }
+static lm_vec2  lm_sub2      (lm_vec2 a, lm_vec2 b) { return lm_v2(a.x - b.x, a.y - b.y); }
+static lm_vec2  lm_scale2    (lm_vec2 a, float   b) { return lm_v2(a.x * b, a.y * b); }
+static lm_vec2  lm_div2      (lm_vec2 a, float   b) { return lm_scale2(a, 1.0f / b); }
+static lm_vec2  lm_min2      (lm_vec2 a, lm_vec2 b) { return lm_v2(bx::min(a.x, b.x), bx::min(a.y, b.y)); }
+static lm_vec2  lm_max2      (lm_vec2 a, lm_vec2 b) { return lm_v2(bx::max(a.x, b.x), bx::max(a.y, b.y)); }
+static lm_vec2  lm_floor2    (lm_vec2 a           ) { return lm_v2(floorf(a.x), floorf(a.y)); }
+static lm_vec2  lm_ceil2     (lm_vec2 a           ) { return lm_v2(ceilf (a.x), ceilf (a.y)); }
+static float    lm_dot2      (lm_vec2 a, lm_vec2 b) { return a.x * b.x + a.y * b.y; }
+static float    lm_cross2    (lm_vec2 a, lm_vec2 b) { return a.x * b.y - a.y * b.x; } // pseudo cross product
 
 static lm_vec2 lm_toBarycentric(lm_vec2 p1, lm_vec2 p2, lm_vec2 p3, lm_vec2 p)
 {
@@ -348,20 +299,20 @@ static inline int lm_leftOf(lm_vec2 a, lm_vec2 b, lm_vec2 c)
 	return x < 0 ? -1 : x > 0;
 }
 
-static lm_bool lm_lineIntersection(lm_vec2 x0, lm_vec2 x1, lm_vec2 y0, lm_vec2 y1, lm_vec2* res)
+static bool lm_lineIntersection(lm_vec2 x0, lm_vec2 x1, lm_vec2 y0, lm_vec2 y1, lm_vec2* res)
 {
 	lm_vec2 dx = lm_sub2(x1, x0);
 	lm_vec2 dy = lm_sub2(y1, y0);
 	lm_vec2 d = lm_sub2(x0, y0);
 	float dyx = lm_cross2(dy, dx);
 	if (dyx == 0.0f)
-		return LM_FALSE;
+		return false;
 	dyx = lm_cross2(d, dx) / dyx;
 	if (dyx <= 0 || dyx >= 1)
-		return LM_FALSE;
+		return false;
 	res->x = y0.x + dyx * dy.x;
 	res->y = y0.y + dyx * dy.y;
-	return LM_TRUE;
+	return true;
 }
 
 // this modifies the poly array! the poly array must be big enough to hold the result!
@@ -402,7 +353,7 @@ struct lm_context
 {
 	struct
 	{
-		lm_vec3 p[3];
+		bx::Vec3 p[3];
 		lm_vec2 uv[3];
 	} triangle;
 
@@ -415,12 +366,12 @@ struct lm_context
 
 	struct
 	{
-		lm_vec3 position;
-		lm_vec3 direction;
+		bx::Vec3 position;
+		bx::Vec3 direction;
 	} sample;
 };
 
-static lm_bool lm_hasConservativeTriangleRasterizerFinished(lm_context *ctx)
+static bool lm_hasConservativeTriangleRasterizerFinished(lm_context *ctx)
 {
 	return ctx->rasterizer.y >= ctx->rasterizer.maxy;
 }
@@ -434,10 +385,10 @@ static void lm_moveToNextPotentialConservativeTriangleRasterizerPosition(lm_cont
 	}
 }
 
-static lm_bool lm_trySamplingConservativeTriangleRasterizerPosition(lm_context *ctx)
+static bool lm_trySamplingConservativeTriangleRasterizerPosition(lm_context *ctx)
 {
 	if (lm_hasConservativeTriangleRasterizerFinished(ctx))
-		return LM_FALSE;
+		return false;
 
 	lm_vec2 pixel[16];
 	pixel[0] = lm_v2i(ctx->rasterizer.x, ctx->rasterizer.y);
@@ -458,7 +409,7 @@ static lm_bool lm_trySamplingConservativeTriangleRasterizerPosition(lm_context *
 			area += res[i - 1].x * res[i].y - res[i - 1].y * res[i].x;
 		}
 		centroid = lm_div2(centroid, (float)nRes);
-		area = lm_absf(area / 2.0f);
+		area = bx::abs(area / 2.0f);
 
 		if (area > 0.0f)
 		{
@@ -470,42 +421,42 @@ static lm_bool lm_trySamplingConservativeTriangleRasterizerPosition(lm_context *
 				centroid);
 
 			// sample it only if its's not degenerate
-			if (lm_finite2(uv))
+			if (bx::isFinite(uv.x) && bx::isFinite(uv.y))
 			{
-				lm_vec3 p0 = ctx->triangle.p[0];
-				lm_vec3 p1 = ctx->triangle.p[1];
-				lm_vec3 p2 = ctx->triangle.p[2];
-				lm_vec3 v1 = lm_sub3(p1, p0);
-				lm_vec3 v2 = lm_sub3(p2, p0);
-				ctx->sample.position = lm_add3(p0, lm_add3(lm_scale3(v2, uv.x), lm_scale3(v1, uv.y)));
-				ctx->sample.direction = lm_normalize3(lm_cross3(v1, v2));
+				bx::Vec3 p0 = ctx->triangle.p[0];
+				bx::Vec3 p1 = ctx->triangle.p[1];
+				bx::Vec3 p2 = ctx->triangle.p[2];
+				bx::Vec3 v1 = bx::sub(p1, p0);
+				bx::Vec3 v2 = bx::sub(p2, p0);
+				ctx->sample.position = bx::add(p0, bx::add(bx::mul(v2, uv.x), bx::mul(v1, uv.y)));
+				ctx->sample.direction = bx::normalize(bx::cross(v1, v2));
 
-				if (lm_finite3(ctx->sample.position) &&
-					lm_finite3(ctx->sample.direction) &&
-					lm_length3sq(ctx->sample.direction) > 0.5f) // don't allow 0.0f. should always be ~1.0f
+				if (bx::isFinite(ctx->sample.position.x) && bx::isFinite(ctx->sample.position.y) && bx::isFinite(ctx->sample.position.z) &&
+					bx::isFinite(ctx->sample.direction.x) && bx::isFinite(ctx->sample.direction.y) && bx::isFinite(ctx->sample.direction.z) &&
+					bx::length(ctx->sample.direction) > 0.5f) // don't allow 0.0f. should always be ~1.0f
 				{
-					return LM_TRUE;
+					return true;
 				}
 			}
 		}
 	}
-	return LM_FALSE;
+	return false;
 }
 
 // returns true if a sampling position was found and
 // false if we finished rasterizing the current triangle
-static lm_bool lm_findFirstConservativeTriangleRasterizerPosition(lm_context *ctx)
+static bool lm_findFirstConservativeTriangleRasterizerPosition(lm_context *ctx)
 {
 	while (!lm_trySamplingConservativeTriangleRasterizerPosition(ctx))
 	{
 		lm_moveToNextPotentialConservativeTriangleRasterizerPosition(ctx);
 		if (lm_hasConservativeTriangleRasterizerFinished(ctx))
-			return LM_FALSE;
+			return false;
 	}
-	return LM_TRUE;
+	return true;
 }
 
-static lm_bool lm_findNextConservativeTriangleRasterizerPosition(lm_context *ctx)
+static bool lm_findNextConservativeTriangleRasterizerPosition(lm_context *ctx)
 {
 	lm_moveToNextPotentialConservativeTriangleRasterizerPosition(ctx);
 	return lm_findFirstConservativeTriangleRasterizerPosition(ctx);
@@ -525,9 +476,7 @@ static bool bakeRasterize()
 		lm_vec2 uvMin = lm_v2(FLT_MAX, FLT_MAX), uvMax = lm_v2(-FLT_MAX, -FLT_MAX);
 		for (int i = 0; i < 3; i++) {
 			const ModelVertex &vertex = modelVertices[modelIndices[tri * 3 + i]];
-			ctx.triangle.p[i].x = vertex.pos.x;
-			ctx.triangle.p[i].y = vertex.pos.y;
-			ctx.triangle.p[i].z = vertex.pos.z;
+			ctx.triangle.p[i] = vertex.pos;
 			ctx.triangle.uv[i].x = vertex.texcoord[2] * s_bake.lightmapWidth;
 			ctx.triangle.uv[i].y = vertex.texcoord[3] * s_bake.lightmapHeight;
 			// update bounds on lightmap
@@ -537,16 +486,16 @@ static bool bakeRasterize()
 		// Calculate area of interest (on lightmap) for conservative rasterization.
 		lm_vec2 bbMin = lm_floor2(uvMin);
 		lm_vec2 bbMax = lm_ceil2(uvMax);
-		ctx.rasterizer.minx = ctx.rasterizer.x = lm_maxi((int)bbMin.x - 1, 0);
-		ctx.rasterizer.miny = ctx.rasterizer.y = lm_maxi((int)bbMin.y - 1, 0);
-		ctx.rasterizer.maxx = lm_mini((int)bbMax.x + 1, (int)s_bake.lightmapWidth - 1);
-		ctx.rasterizer.maxy = lm_mini((int)bbMax.y + 1, (int)s_bake.lightmapHeight - 1);
+		ctx.rasterizer.minx = ctx.rasterizer.x = bx::max((int)bbMin.x - 1, 0);
+		ctx.rasterizer.miny = ctx.rasterizer.y = bx::max((int)bbMin.y - 1, 0);
+		ctx.rasterizer.maxx = bx::min((int)bbMax.x + 1, (int)s_bake.lightmapWidth - 1);
+		ctx.rasterizer.maxy = bx::min((int)bbMax.y + 1, (int)s_bake.lightmapHeight - 1);
 		assert(ctx.rasterizer.minx <= ctx.rasterizer.maxx && ctx.rasterizer.miny <= ctx.rasterizer.maxy);
 		if (lm_findFirstConservativeTriangleRasterizerPosition(&ctx)) {
 			for (;;) {
 				SampleLocation sample;
-				sample.pos = bx::Vec3(ctx.sample.position.x, ctx.sample.position.y, ctx.sample.position.z);
-				sample.normal = bx::Vec3(ctx.sample.direction.x, ctx.sample.direction.y, ctx.sample.direction.z);
+				sample.pos = ctx.sample.position;
+				sample.normal = ctx.sample.direction;
 				sample.uv[0] = ctx.rasterizer.x;
 				sample.uv[1] = ctx.rasterizer.y;
 				s_bake.sampleLocations.push_back(sample);
