82bbb2497d64df9196b0442a406b86946319f89a
Jonathan Young <young.jpc@gmail.com>
2020-02-16 21:12:40 +1000

    Remove dgemv.

--- extra/thirdparty/OpenNL/OpenNL_psm.c
+++ extra/thirdparty/OpenNL/OpenNL_psm.c
@@ -250,13 +250,6 @@ typedef void (*FUNPTR_daxpy)(
     double a, const double *x, int incx, double *y, int incy
 );
 
-
-typedef void (*FUNPTR_dgemv)( 
-    NLBlas_t blas, MatrixTranspose trans, int m, int n, double alpha,
-    const double *A, int ldA, const double *x, int incx,
-    double beta, double *y, int incy 
-);
-
 struct NLBlas {
     FUNPTR_malloc Malloc;
     FUNPTR_free Free;
@@ -267,7 +260,6 @@ struct NLBlas {
     FUNPTR_ddot  Ddot;
     FUNPTR_dnrm2 Dnrm2;
     FUNPTR_daxpy Daxpy;
-    FUNPTR_dgemv Dgemv;
 
     NLboolean has_unified_memory;
     double start_time;
@@ -1367,8 +1359,7 @@ int NL_FORTRAN_WRAP(xerbla)(char *srname, int *info) {
  * dscal
  * dnrm2
  * dcopy
- * dgemv
-  */
+   */
 
 
 
@@ -1934,304 +1925,6 @@ L40:
 #undef DX
 #undef DY
 
-/* Subroutine */ static int NL_FORTRAN_WRAP(dgemv)(const char *trans, integer *m, integer *n, doublereal *
-        alpha, doublereal *a, integer *lda, doublereal *x, integer *incx, 
-        doublereal *beta, doublereal *y, integer *incy)
-{
-
-
-    /* System generated locals */
-    /* integer a_dim1, a_offset ; */
-    integer i__1, i__2; 
-
-    /* Local variables */
-    static integer info;
-    static doublereal temp;
-    static integer lenx, leny, i, j;
-/*    extern logical lsame_(char *, char *); */
-    static integer ix, iy, jx, jy, kx, ky;
-/*    extern int xerbla_(char *, integer *); */
-
-
-/*  Purpose   
-    =======   
-
-    DGEMV  performs one of the matrix-vector operations   
-
-       y := alpha*A*x + beta*y,   or   y := alpha*A'*x + beta*y,   
-
-    where alpha and beta are scalars, x and y are vectors and A is an   
-    m by n matrix.   
-
-    Parameters   
-    ==========   
-
-    TRANS  - CHARACTER*1.   
-             On entry, TRANS specifies the operation to be performed as   
-             follows:   
-
-                TRANS = 'N' or 'n'   y := alpha*A*x + beta*y.   
-
-                TRANS = 'T' or 't'   y := alpha*A'*x + beta*y.   
-
-                TRANS = 'C' or 'c'   y := alpha*A'*x + beta*y.   
-
-             Unchanged on exit.   
-
-    M      - INTEGER.   
-             On entry, M specifies the number of rows of the matrix A.   
-             M must be at least zero.   
-             Unchanged on exit.   
-
-    N      - INTEGER.   
-             On entry, N specifies the number of columns of the matrix A. 
-  
-             N must be at least zero.   
-             Unchanged on exit.   
-
-    ALPHA  - DOUBLE PRECISION.   
-             On entry, ALPHA specifies the scalar alpha.   
-             Unchanged on exit.   
-
-    A      - DOUBLE PRECISION array of DIMENSION ( LDA, n ).   
-             Before entry, the leading m by n part of the array A must   
-             contain the matrix of coefficients.   
-             Unchanged on exit.   
-
-    LDA    - INTEGER.   
-             On entry, LDA specifies the first dimension of A as declared 
-  
-             in the calling (sub) program. LDA must be at least   
-             max( 1, m ).   
-             Unchanged on exit.   
-
-    X      - DOUBLE PRECISION array of DIMENSION at least   
-             ( 1 + ( n - 1 )*abs( INCX ) ) when TRANS = 'N' or 'n'   
-             and at least   
-             ( 1 + ( m - 1 )*abs( INCX ) ) otherwise.   
-             Before entry, the incremented array X must contain the   
-             vector x.   
-             Unchanged on exit.   
-
-    INCX   - INTEGER.   
-             On entry, INCX specifies the increment for the elements of   
-             X. INCX must not be zero.   
-             Unchanged on exit.   
-
-    BETA   - DOUBLE PRECISION.   
-             On entry, BETA specifies the scalar beta. When BETA is   
-             supplied as zero then Y need not be set on input.   
-             Unchanged on exit.   
-
-    Y      - DOUBLE PRECISION array of DIMENSION at least   
-             ( 1 + ( m - 1 )*abs( INCY ) ) when TRANS = 'N' or 'n'   
-             and at least   
-             ( 1 + ( n - 1 )*abs( INCY ) ) otherwise.   
-             Before entry with BETA non-zero, the incremented array Y   
-             must contain the vector y. On exit, Y is overwritten by the 
-  
-             updated vector y.   
-
-    INCY   - INTEGER.   
-             On entry, INCY specifies the increment for the elements of   
-             Y. INCY must not be zero.   
-             Unchanged on exit.   
-
-
-    Level 2 Blas routine.   
-
-    -- Written on 22-October-1986.   
-       Jack Dongarra, Argonne National Lab.   
-       Jeremy Du Croz, Nag Central Office.   
-       Sven Hammarling, Nag Central Office.   
-       Richard Hanson, Sandia National Labs.   
-
-
-
-       Test the input parameters.   
-
-    
-   Parameter adjustments   
-       Function Body */
-#define X(I) x[(I)-1]
-#define Y(I) y[(I)-1]
-
-#define A(I,J) a[(I)-1 + ((J)-1)* ( *lda)]
-
-    info = 0;
-    if (! NL_FORTRAN_WRAP(lsame)(trans, "N") && ! NL_FORTRAN_WRAP(lsame)(trans, "T") && ! 
-            NL_FORTRAN_WRAP(lsame)(trans, "C")) {
-        info = 1;
-    } else if (*m < 0) {
-        info = 2;
-    } else if (*n < 0) {
-        info = 3;
-    } else if (*lda < max(1,*m)) {
-        info = 6;
-    } else if (*incx == 0) {
-        info = 8;
-    } else if (*incy == 0) {
-        info = 11;
-    }
-    if (info != 0) {
-        NL_FORTRAN_WRAP(xerbla)("DGEMV ", &info);
-        return 0;
-    }
-
-/*     Quick return if possible. */
-
-    if (*m == 0 || *n == 0 || (*alpha == 0. && *beta == 1.)) {
-        return 0;
-    }
-
-/*     Set  LENX  and  LENY, the lengths of the vectors x and y, and set 
-  
-       up the start points in  X  and  Y. */
-
-    if (NL_FORTRAN_WRAP(lsame)(trans, "N")) {
-        lenx = *n;
-        leny = *m;
-    } else {
-        lenx = *m;
-        leny = *n;
-    }
-    if (*incx > 0) {
-        kx = 1;
-    } else {
-        kx = 1 - (lenx - 1) * *incx;
-    }
-    if (*incy > 0) {
-        ky = 1;
-    } else {
-        ky = 1 - (leny - 1) * *incy;
-    }
-
-/*     Start the operations. In this version the elements of A are   
-       accessed sequentially with one pass through A.   
-
-       First form  y := beta*y. */
-
-    if (*beta != 1.) {
-        if (*incy == 1) {
-            if (*beta == 0.) {
-                i__1 = leny;
-                for (i = 1; i <= leny; ++i) {
-                    Y(i) = 0.;
-/* L10: */
-                }
-            } else {
-                i__1 = leny;
-                for (i = 1; i <= leny; ++i) {
-                    Y(i) = *beta * Y(i);
-/* L20: */
-                }
-            }
-        } else {
-            iy = ky;
-            if (*beta == 0.) {
-                i__1 = leny;
-                for (i = 1; i <= leny; ++i) {
-                    Y(iy) = 0.;
-                    iy += *incy;
-/* L30: */
-                }
-            } else {
-                i__1 = leny;
-                for (i = 1; i <= leny; ++i) {
-                    Y(iy) = *beta * Y(iy);
-                    iy += *incy;
-/* L40: */
-                }
-            }
-        }
-    }
-    if (*alpha == 0.) {
-        return 0;
-    }
-    if (NL_FORTRAN_WRAP(lsame)(trans, "N")) {
-
-/*        Form  y := alpha*A*x + y. */
-
-        jx = kx;
-        if (*incy == 1) {
-            i__1 = *n;
-            for (j = 1; j <= *n; ++j) {
-                if (X(jx) != 0.) {
-                    temp = *alpha * X(jx);
-                    i__2 = *m;
-                    for (i = 1; i <= *m; ++i) {
-                        Y(i) += temp * A(i,j);
-/* L50: */
-                    }
-                }
-                jx += *incx;
-/* L60: */
-            }
-        } else {
-            i__1 = *n;
-            for (j = 1; j <= *n; ++j) {
-                if (X(jx) != 0.) {
-                    temp = *alpha * X(jx);
-                    iy = ky;
-                    i__2 = *m;
-                    for (i = 1; i <= *m; ++i) {
-                        Y(iy) += temp * A(i,j);
-                        iy += *incy;
-/* L70: */
-                    }
-                }
-                jx += *incx;
-/* L80: */
-            }
-        }
-    } else {
-
-/*        Form  y := alpha*A'*x + y. */
-
-        jy = ky;
-        if (*incx == 1) {
-            i__1 = *n;
-            for (j = 1; j <= *n; ++j) {
-                temp = 0.;
-                i__2 = *m;
-                for (i = 1; i <= *m; ++i) {
-                    temp += A(i,j) * X(i);
-/* L90: */
-                }
-                Y(jy) += *alpha * temp;
-                jy += *incy;
-/* L100: */
-            }
-        } else {
-            i__1 = *n;
-            for (j = 1; j <= *n; ++j) {
-                temp = 0.;
-                ix = kx;
-                i__2 = *m;
-                for (i = 1; i <= *m; ++i) {
-                    temp += A(i,j) * X(ix);
-                    ix += *incx;
-/* L110: */
-                }
-                Y(jy) += *alpha * temp;
-                jy += *incy;
-/* L120: */
-            }
-        }
-    }
-
-    nl_arg_used(i__1);
-    nl_arg_used(i__2);
-    return 0;
-
-/*     End of DGEMV . */
-
-} /* dgemv_ */
-
-#undef X
-#undef Y
-#undef A
-
 /* End of BLAS routines */
 
 
@@ -2334,20 +2027,6 @@ static void host_blas_dscal(
     NL_FORTRAN_WRAP(dscal)(&n,&a,x,&incx);    
 }
 
-static void host_blas_dgemv(
-    NLBlas_t blas, MatrixTranspose trans, int m, int n, double alpha,
-    const double *A, int ldA, const double *x, int incx,
-    double beta, double *y, int incy 
-) {
-    static const char *T[3] = { "N", "T", 0 };
-    nl_arg_used(blas);
-    NL_FORTRAN_WRAP(dgemv)(
-	T[(int)trans],&m,&n,&alpha,(double*)A,&ldA,
-	(double*)x,&incx,&beta,y,&incy
-    );
-    /* TODO: update flops */    
-}
-
 NLBlas_t nlHostBlas() {
     static NLboolean initialized = NL_FALSE;
     static struct NLBlas blas;
@@ -2362,7 +2041,6 @@ NLBlas_t nlHostBlas() {
 	blas.Dnrm2 = host_blas_dnrm2;
 	blas.Daxpy = host_blas_daxpy;
 	blas.Dscal = host_blas_dscal;
-	blas.Dgemv = host_blas_dgemv;
 	nlBlasResetStats(&blas);
 	initialized = NL_TRUE;
     }
