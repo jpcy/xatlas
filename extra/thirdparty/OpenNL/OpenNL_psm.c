6c45914e9c04afc77a14126b0a290b7f92c52197
Jonathan Young <young.jpc@gmail.com>
2020-02-16 21:10:32 +1000

    Remove dtpsv.

--- extra/thirdparty/OpenNL/OpenNL_psm.c
+++ extra/thirdparty/OpenNL/OpenNL_psm.c
@@ -208,10 +208,6 @@ typedef enum {
 } MatrixTranspose ;
 
 typedef enum {
-    UpperTriangle=0, LowerTriangle=1
-} MatrixTriangle ;
-
-typedef enum {
     UnitTriangular=0, NotUnitTriangular=1
 } MatrixUnitTriangular ;
 
@@ -261,13 +257,6 @@ typedef void (*FUNPTR_dgemv)(
     double beta, double *y, int incy 
 );
 
-
-typedef void (*FUNPTR_dtpsv)(
-    NLBlas_t blas, MatrixTriangle uplo, MatrixTranspose trans,
-    MatrixUnitTriangular diag, int n, const double *AP,
-    double *x, int incx 
-);
-
 struct NLBlas {
     FUNPTR_malloc Malloc;
     FUNPTR_free Free;
@@ -279,7 +268,6 @@ struct NLBlas {
     FUNPTR_dnrm2 Dnrm2;
     FUNPTR_daxpy Daxpy;
     FUNPTR_dgemv Dgemv;
-    FUNPTR_dtpsv Dtpsv;
 
     NLboolean has_unified_memory;
     double start_time;
@@ -1366,16 +1354,7 @@ int NL_FORTRAN_WRAP(xerbla)(char *srname, int *info) {
     );
     return 0;
 } 
-#ifndef NL_USE_BLAS
-#define NL_USE_BLAS
 #endif
-#endif
-
-#ifndef NL_USE_BLAS
-#define NEEDS_DTPSV
-#endif
-
-
 
 /* BLAS routines                                                           */
 /* copy-pasted from CBLAS (i.e. generated from f2c) */
@@ -1389,8 +1368,7 @@ int NL_FORTRAN_WRAP(xerbla)(char *srname, int *info) {
  * dnrm2
  * dcopy
  * dgemv
- * dtpsv
- */
+  */
 
 
 
@@ -1403,8 +1381,6 @@ typedef NLint     ftnlen ;
 #ifndef max
 #define max(x,y) ((x) > (y) ? (x) : (y))
 #endif
-
-#ifndef NL_USE_BLAS
 
 static int NL_FORTRAN_WRAP(lsame)(const char *ca, const char *cb)
 {
@@ -2256,410 +2232,6 @@ L40:
 #undef Y
 #undef A
 
-
-
-
-#else
-
-extern void NL_FORTRAN_WRAP(daxpy)( 
-    int *n, double *alpha, double *x,
-    int *incx, double *y, int *incy 
-) ;
-
-
-extern double NL_FORTRAN_WRAP(dnrm2)( int *n, double *x, int *incx ) ;
-
-extern int NL_FORTRAN_WRAP(dcopy)(int* n, double* dx, int* incx, double* dy, int* incy) ;
-
-extern void NL_FORTRAN_WRAP(dscal)(int* n, double* alpha, double *x, int* incx) ;
-
-#ifndef NEEDS_DTPSV
-extern void NL_FORTRAN_WRAP(dtpsv)( 
-    char *uplo, char *trans, char *diag,
-    int *n, double *AP, double *x, int *incx 
-) ;
-#endif
-
-extern void NL_FORTRAN_WRAP(dgemv)( 
-    char *trans, int *m, int *n,
-    double *alpha, double *A, int *ldA,
-    double *x, int *incx,
-    double *beta, double *y, int *incy 
-) ;
-
-#endif
-
-#ifdef NEEDS_DTPSV
-
-/* DECK DTPSV */
-/* Subroutine */ 
-static int NL_FORTRAN_WRAP(dtpsv)(
-   const char* uplo, 
-   const char* trans, 
-   const char* diag, 
-   integer* n, 
-   doublereal* ap, 
-   doublereal* x, 
-   integer* incx
-) {
-    /* System generated locals */
-    integer i__1, i__2;
-
-    /* Local variables */
-    static integer info;
-    static doublereal temp;
-    static integer i__, j, k;
-/*    extern logical lsame_(); */
-    static integer kk, ix, jx, kx;
-/*    extern int xerbla_(); */
-    static logical nounit;
-
-/* ***BEGIN PROLOGUE  DTPSV */
-/* ***PURPOSE  Solve one of the systems of equations. */
-/* ***LIBRARY   SLATEC (BLAS) */
-/* ***CATEGORY  D1B4 */
-/* ***TYPE      DOUBLE PRECISION (STPSV-S, DTPSV-D, CTPSV-C) */
-/* ***KEYWORDS  LEVEL 2 BLAS, LINEAR ALGEBRA */
-/* ***AUTHOR  Dongarra, J. J., (ANL) */
-/*           Du Croz, J., (NAG) */
-/*           Hammarling, S., (NAG) */
-/*           Hanson, R. J., (SNLA) */
-/* ***DESCRIPTION */
-
-/*  DTPSV  solves one of the systems of equations */
-
-/*     A*x = b,   or   A'*x = b, */
-
-/*  where b and x are n element vectors and A is an n by n unit, or */
-/*  non-unit, upper or lower triangular matrix, supplied in packed form. */
-
-/*  No test for singularity or near-singularity is included in this */
-/*  routine. Such tests must be performed before calling this routine. */
-
-/*  Parameters */
-/*  ========== */
-
-/*  UPLO   - CHARACTER*1. */
-/*           On entry, UPLO specifies whether the matrix is an upper or */
-/*           lower triangular matrix as follows: */
-
-/*              UPLO = 'U' or 'u'   A is an upper triangular matrix. */
-
-/*              UPLO = 'L' or 'l'   A is a lower triangular matrix. */
-
-/*           Unchanged on exit. */
-
-/*  TRANS  - CHARACTER*1. */
-/*           On entry, TRANS specifies the equations to be solved as */
-/*           follows: */
-
-/*              TRANS = 'N' or 'n'   A*x = b. */
-
-/*              TRANS = 'T' or 't'   A'*x = b. */
-
-/*              TRANS = 'C' or 'c'   A'*x = b. */
-
-/*           Unchanged on exit. */
-
-/*  DIAG   - CHARACTER*1. */
-/*           On entry, DIAG specifies whether or not A is unit */
-/*           triangular as follows: */
-
-/*              DIAG = 'U' or 'u'   A is assumed to be unit triangular. */
-
-/*              DIAG = 'N' or 'n'   A is not assumed to be unit */
-/*                                  triangular. */
-
-/*           Unchanged on exit. */
-
-/*  N      - INTEGER. */
-/*           On entry, N specifies the order of the matrix A. */
-/*           N must be at least zero. */
-/*           Unchanged on exit. */
-
-/*  AP     - DOUBLE PRECISION array of DIMENSION at least */
-/*           ( ( n*( n + 1))/2). */
-/*           Before entry with  UPLO = 'U' or 'u', the array AP must */
-/*           contain the upper triangular matrix packed sequentially, */
-/*           column by column, so that AP( 1 ) contains a( 1, 1 ), */
-/*           AP( 2 ) and AP( 3 ) contain a( 1, 2 ) and a( 2, 2 ) */
-/*           respectively, and so on. */
-/*           Before entry with UPLO = 'L' or 'l', the array AP must */
-/*           contain the lower triangular matrix packed sequentially, */
-/*           column by column, so that AP( 1 ) contains a( 1, 1 ), */
-/*           AP( 2 ) and AP( 3 ) contain a( 2, 1 ) and a( 3, 1 ) */
-/*           respectively, and so on. */
-/*           Note that when  DIAG = 'U' or 'u', the diagonal elements of */
-/*           A are not referenced, but are assumed to be unity. */
-/*           Unchanged on exit. */
-
-/*  X      - DOUBLE PRECISION array of dimension at least */
-/*           ( 1 + ( n - 1 )*abs( INCX ) ). */
-/*           Before entry, the incremented array X must contain the n */
-/*           element right-hand side vector b. On exit, X is overwritten */
-/*           with the solution vector x. */
-
-/*  INCX   - INTEGER. */
-/*           On entry, INCX specifies the increment for the elements of */
-/*           X. INCX must not be zero. */
-/*           Unchanged on exit. */
-
-/* ***REFERENCES  Dongarra, J. J., Du Croz, J., Hammarling, S., and */
-/*                 Hanson, R. J.  An extended set of Fortran basic linear */
-/*                 algebra subprograms.  ACM TOMS, Vol. 14, No. 1, */
-/*                 pp. 1-17, March 1988. */
-/* ***ROUTINES CALLED  LSAME, XERBLA */
-/* ***REVISION HISTORY  (YYMMDD) */
-/*   861022  DATE WRITTEN */
-/*   910605  Modified to meet SLATEC prologue standards.  Only comment */
-/*           lines were modified.  (BKS) */
-/* ***END PROLOGUE  DTPSV */
-/*     .. Scalar Arguments .. */
-/*     .. Array Arguments .. */
-/*     .. Parameters .. */
-/*     .. Local Scalars .. */
-/*     .. External Functions .. */
-/*     .. External Subroutines .. */
-/* ***FIRST EXECUTABLE STATEMENT  DTPSV */
-
-/*     Test the input parameters. */
-
-    /* Parameter adjustments */
-    --x;
-    --ap;
-
-    /* Function Body */
-    info = 0;
-    if (!NL_FORTRAN_WRAP(lsame)(uplo, "U") && 
-        !NL_FORTRAN_WRAP(lsame)(uplo, "L")
-    ) {
-        info = 1;
-    } else if (
-        !NL_FORTRAN_WRAP(lsame)(trans, "N") && 
-        !NL_FORTRAN_WRAP(lsame)(trans, "T") && 
-        !NL_FORTRAN_WRAP(lsame)(trans, "C")
-    ) {
-        info = 2;
-    } else if (
-        !NL_FORTRAN_WRAP(lsame)(diag, "U") && 
-        !NL_FORTRAN_WRAP(lsame)(diag, "N")
-    ) {
-        info = 3;
-    } else if (*n < 0) {
-        info = 4;
-    } else if (*incx == 0) {
-        info = 7;
-    }
-    if (info != 0) {
-        NL_FORTRAN_WRAP(xerbla)("DTPSV ", &info);
-        return 0;
-    }
-
-/*     Quick return if possible. */
-
-    if (*n == 0) {
-        return 0;
-    }
-
-    nounit = (logical)(NL_FORTRAN_WRAP(lsame)(diag, "N"));
-
-/*     Set up the start point in X if the increment is not unity. This */
-/*     will be  ( N - 1 )*INCX  too small for descending loops. */
-
-    if (*incx <= 0) {
-        kx = 1 - (*n - 1) * *incx;
-    } else if (*incx != 1) {
-        kx = 1;
-    }
-
-/*     Start the operations. In this version the elements of AP are */
-/*     accessed sequentially with one pass through AP. */
-
-    if (NL_FORTRAN_WRAP(lsame)(trans, "N")) {
-
-/*        Form  x := inv( A )*x. */
-
-        if (NL_FORTRAN_WRAP(lsame)(uplo, "U")) {
-            kk = *n * (*n + 1) / 2;
-            if (*incx == 1) {
-                for (j = *n; j >= 1; --j) {
-                    if (x[j] != 0.) {
-                        if (nounit) {
-                            x[j] /= ap[kk];
-                        }
-                        temp = x[j];
-                        k = kk - 1;
-                        for (i__ = j - 1; i__ >= 1; --i__) {
-                            x[i__] -= temp * ap[k];
-                            --k;
-/* L10: */
-                        }
-                    }
-                    kk -= j;
-/* L20: */
-                }
-            } else {
-                jx = kx + (*n - 1) * *incx;
-                for (j = *n; j >= 1; --j) {
-                    if (x[jx] != 0.) {
-                        if (nounit) {
-                            x[jx] /= ap[kk];
-                        }
-                        temp = x[jx];
-                        ix = jx;
-                        i__1 = kk - j + 1;
-                        for (k = kk - 1; k >= i__1; --k) {
-                            ix -= *incx;
-                            x[ix] -= temp * ap[k];
-/* L30: */
-                        }
-                    }
-                    jx -= *incx;
-                    kk -= j;
-/* L40: */
-                }
-            }
-        } else {
-            kk = 1;
-            if (*incx == 1) {
-                i__1 = *n;
-                for (j = 1; j <= i__1; ++j) {
-                    if (x[j] != 0.) {
-                        if (nounit) {
-                            x[j] /= ap[kk];
-                        }
-                        temp = x[j];
-                        k = kk + 1;
-                        i__2 = *n;
-                        for (i__ = j + 1; i__ <= i__2; ++i__) {
-                            x[i__] -= temp * ap[k];
-                            ++k;
-/* L50: */
-                        }
-                    }
-                    kk += *n - j + 1;
-/* L60: */
-                }
-            } else {
-                jx = kx;
-                i__1 = *n;
-                for (j = 1; j <= i__1; ++j) {
-                    if (x[jx] != 0.) {
-                        if (nounit) {
-                            x[jx] /= ap[kk];
-                        }
-                        temp = x[jx];
-                        ix = jx;
-                        i__2 = kk + *n - j;
-                        for (k = kk + 1; k <= i__2; ++k) {
-                            ix += *incx;
-                            x[ix] -= temp * ap[k];
-/* L70: */
-                        }
-                    }
-                    jx += *incx;
-                    kk += *n - j + 1;
-/* L80: */
-                }
-            }
-        }
-    } else {
-
-/*        Form  x := inv( A' )*x. */
-
-        if (NL_FORTRAN_WRAP(lsame)(uplo, "U")) {
-            kk = 1;
-            if (*incx == 1) {
-                i__1 = *n;
-                for (j = 1; j <= i__1; ++j) {
-                    temp = x[j];
-                    k = kk;
-                    i__2 = j - 1;
-                    for (i__ = 1; i__ <= i__2; ++i__) {
-                        temp -= ap[k] * x[i__];
-                        ++k;
-/* L90: */
-                    }
-                    if (nounit) {
-                        temp /= ap[kk + j - 1];
-                    }
-                    x[j] = temp;
-                    kk += j;
-/* L100: */
-                }
-            } else {
-                jx = kx;
-                i__1 = *n;
-                for (j = 1; j <= i__1; ++j) {
-                    temp = x[jx];
-                    ix = kx;
-                    i__2 = kk + j - 2;
-                    for (k = kk; k <= i__2; ++k) {
-                        temp -= ap[k] * x[ix];
-                        ix += *incx;
-/* L110: */
-                    }
-                    if (nounit) {
-                        temp /= ap[kk + j - 1];
-                    }
-                    x[jx] = temp;
-                    jx += *incx;
-                    kk += j;
-/* L120: */
-                }
-            }
-        } else {
-            kk = *n * (*n + 1) / 2;
-            if (*incx == 1) {
-                for (j = *n; j >= 1; --j) {
-                    temp = x[j];
-                    k = kk;
-                    i__1 = j + 1;
-                    for (i__ = *n; i__ >= i__1; --i__) {
-                        temp -= ap[k] * x[i__];
-                        --k;
-/* L130: */
-                    }
-                    if (nounit) {
-                        temp /= ap[kk - *n + j];
-                    }
-                    x[j] = temp;
-                    kk -= *n - j + 1;
-/* L140: */
-                }
-            } else {
-                kx += (*n - 1) * *incx;
-                jx = kx;
-                for (j = *n; j >= 1; --j) {
-                    temp = x[jx];
-                    ix = kx;
-                    i__1 = kk - (*n - (j + 1));
-                    for (k = kk; k >= i__1; --k) {
-                        temp -= ap[k] * x[ix];
-                        ix -= *incx;
-/* L150: */
-                    }
-                    if (nounit) {
-                        temp /= ap[kk - *n + j];
-                    }
-                    x[jx] = temp;
-                    jx -= *incx;
-                    kk -= *n - j + 1;
-/* L160: */
-                }
-            }
-        }
-    }
-
-    return 0;
-
-/*     End of DTPSV . */
-
-} /* dtpsv_ */
-
-#endif 
-
-
 /* End of BLAS routines */
 
 
@@ -2776,21 +2348,6 @@ static void host_blas_dgemv(
     /* TODO: update flops */    
 }
 
-static void host_blas_dtpsv(
-    NLBlas_t blas, MatrixTriangle uplo, MatrixTranspose trans,
-    MatrixUnitTriangular diag, int n, const double *AP,
-    double *x, int incx 
-) {
-    static const char *UL[2] = { "U", "L" };
-    static const char *T[3]  = { "N", "T", 0 };
-    static const char *D[2]  = { "U", "N" };
-    nl_arg_used(blas);    
-    NL_FORTRAN_WRAP(dtpsv)(
-	UL[(int)uplo],T[(int)trans],D[(int)diag],&n,(double*)AP,x,&incx
-    );
-    /* TODO: update flops */
-}
-
 NLBlas_t nlHostBlas() {
     static NLboolean initialized = NL_FALSE;
     static struct NLBlas blas;
@@ -2806,7 +2363,6 @@ NLBlas_t nlHostBlas() {
 	blas.Daxpy = host_blas_daxpy;
 	blas.Dscal = host_blas_dscal;
 	blas.Dgemv = host_blas_dgemv;
-	blas.Dtpsv = host_blas_dtpsv;
 	nlBlasResetStats(&blas);
 	initialized = NL_TRUE;
     }
