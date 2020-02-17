e688a47d8110ce914660248bbe6e93beebfe2260
Jonathan Young <young.jpc@gmail.com>
2020-02-17 12:54:45 +1000

    Replace dscal.

--- extra/thirdparty/OpenNL/OpenNL_psm.c
+++ extra/thirdparty/OpenNL/OpenNL_psm.c
@@ -1337,8 +1337,7 @@ static NLboolean nlSolveIterative() {
 /*
  * daxpy
  * ddot
- * dscal
-    */
+     */
 
 
 
@@ -1532,90 +1531,7 @@ L60:
 #undef DY
 #undef DX
 
-/* Subroutine */ static int NL_FORTRAN_WRAP(dscal)(integer *n, doublereal *da, doublereal *dx, 
-    integer *incx)
-{
-
-
-    /* System generated locals */
-    integer i__1, i__2;
-
-    /* Local variables */
-    static integer i, m, nincx, mp1;
-
-
-/*     scales a vector by a constant.   
-       uses unrolled loops for increment equal to one.   
-       jack dongarra, linpack, 3/11/78.   
-       modified 3/93 to return if incx .le. 0.   
-       modified 12/3/93, array(1) declarations changed to array(*)   
-
-
-    
-   Parameter adjustments   
-       Function Body */
-#ifdef DX
-#undef DX
-#endif
-#define DX(I) dx[(I)-1]
-
-
-    if (*n <= 0 || *incx <= 0) {
-        return 0;
-    }
-    if (*incx == 1) {
-        goto L20;
-    }
-
-/*        code for increment not equal to 1 */
-
-    nincx = *n * *incx;
-    i__1 = nincx;
-    i__2 = *incx;
-    for (i = 1; *incx < 0 ? i >= nincx : i <= nincx; i += *incx) {
-        DX(i) = *da * DX(i);
-/* L10: */
-    }
-    return 0;
-
-/*        code for increment equal to 1   
-
-
-          clean-up loop */
-
-L20:
-    m = *n % 5;
-    if (m == 0) {
-        goto L40;
-    }
-    i__2 = m;
-    for (i = 1; i <= m; ++i) {
-        DX(i) = *da * DX(i);
-/* L30: */
-    }
-    if (*n < 5) {
-        return 0;
-    }
-L40:
-    mp1 = m + 1;
-    i__2 = *n;
-    for (i = mp1; i <= *n; i += 5) {
-        DX(i) = *da * DX(i);
-        DX(i + 1) = *da * DX(i + 1);
-        DX(i + 2) = *da * DX(i + 2);
-        DX(i + 3) = *da * DX(i + 3);
-        DX(i + 4) = *da * DX(i + 4);
-/* L50: */
-    }
-    nl_arg_used(i__1);
-    nl_arg_used(i__2);
-    return 0;
-} /* dscal_ */
-#undef DX
-
 /* End of BLAS routines */
-
-
 
 /* Abstract BLAS interface                                              */
 
@@ -1695,10 +1611,11 @@ static void host_blas_daxpy(
 }
 
 static void host_blas_dscal(
-    NLBlas_t blas, int n, double a, double *x, int incx    
+    NLBlas_t blas, int n, double a, double *x, int incx
 ) {
     blas->flops += (NLulong)n;
-    NL_FORTRAN_WRAP(dscal)(&n,&a,x,&incx);    
+	for (int i = 0; i < n; i++)
+		x[i] *= a;
 }
 
 NLBlas_t nlHostBlas() {
