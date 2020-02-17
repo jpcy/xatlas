c8e7ac4a03050896eaf4cc28feb5b80a31944e65
Jonathan Young <young.jpc@gmail.com>
2020-02-17 13:00:57 +1000

    Replace ddot.

--- extra/thirdparty/OpenNL/OpenNL_psm.c
+++ extra/thirdparty/OpenNL/OpenNL_psm.c
@@ -1331,117 +1331,6 @@ static NLboolean nlSolveIterative() {
 #define NL_FORTRAN_WRAP(x) x##_
 #endif
 
-/* BLAS routines                                                           */
-/* copy-pasted from CBLAS (i.e. generated from f2c) */
-
-/*
- * ddot
-     */
-
-
-
-typedef NLint     integer ;
-typedef NLdouble  doublereal ;
-typedef NLboolean logical ;
-typedef NLint     ftnlen ;
-
-
-#ifndef max
-#define max(x,y) ((x) > (y) ? (x) : (y))
-#endif
-
-static doublereal NL_FORTRAN_WRAP(ddot)(integer *n, doublereal *dx, integer *incx, doublereal *dy, 
-        integer *incy)
-{
-
-    /* System generated locals */
-    integer i__1;
-    doublereal ret_val;
-
-    /* Local variables */
-    static integer i, m;
-    static doublereal dtemp;
-    static integer ix, iy, mp1;
-
-
-/*     forms the dot product of two vectors.   
-       uses unrolled loops for increments equal to one.   
-       jack dongarra, linpack, 3/11/78.   
-       modified 12/3/93, array(1) declarations changed to array(*)   
-
-
-    
-   Parameter adjustments   
-       Function Body */
-#define DY(I) dy[(I)-1]
-#define DX(I) dx[(I)-1]
-
-    ret_val = 0.;
-    dtemp = 0.;
-    if (*n <= 0) {
-        return ret_val;
-    }
-    if (*incx == 1 && *incy == 1) {
-        goto L20;
-    }
-
-/*        code for unequal increments or equal increments   
-            not equal to 1 */
-
-    ix = 1;
-    iy = 1;
-    if (*incx < 0) {
-        ix = (-(*n) + 1) * *incx + 1;
-    }
-    if (*incy < 0) {
-        iy = (-(*n) + 1) * *incy + 1;
-    }
-    i__1 = *n;
-    for (i = 1; i <= *n; ++i) {
-        dtemp += DX(ix) * DY(iy);
-        ix += *incx;
-        iy += *incy;
-/* L10: */
-    }
-    ret_val = dtemp;
-    return ret_val;
-
-/*        code for both increments equal to 1   
-
-
-          clean-up loop */
-
-L20:
-    m = *n % 5;
-    if (m == 0) {
-        goto L40;
-    }
-    i__1 = m;
-    for (i = 1; i <= m; ++i) {
-        dtemp += DX(i) * DY(i);
-/* L30: */
-    }
-    if (*n < 5) {
-        goto L60;
-    }
-L40:
-    mp1 = m + 1;
-    i__1 = *n;
-    for (i = mp1; i <= *n; i += 5) {
-        dtemp = dtemp + DX(i) * DY(i) + DX(i + 1) * DY(i + 1) + DX(i + 2) * 
-                DY(i + 2) + DX(i + 3) * DY(i + 3) + DX(i + 4) * DY(i + 4);
-/* L50: */
-    }
-L60:
-    ret_val = dtemp;
-    nl_arg_used(i__1);
-    return ret_val;
-} /* ddot_ */
-#undef DY
-#undef DX
-
-/* End of BLAS routines */
-
 /* Abstract BLAS interface                                              */
 
 
@@ -1509,7 +1398,10 @@ static double host_blas_ddot(
     NLBlas_t blas, int n, const double *x, int incx, const double *y, int incy    
 ) {
     blas->flops += (NLulong)(2*n);
-    return NL_FORTRAN_WRAP(ddot)(&n,(double*)x,&incx,(double*)y,&incy);
+	double sum = 0.0;
+	for (int i = 0; i < n; i++)
+		sum += x[i] * y[i];
+	return sum;
 }
 
 static void host_blas_daxpy(
