df8dd763c4484182d448d056cec870dbcdd1bb52
Jonathan Young <young.jpc@gmail.com>
2020-07-12 15:51:15 +1000

    Fix trivial clang warning.

--- extra/test.cpp
+++ extra/test.cpp
@@ -218,7 +218,7 @@ int main(int argc, char **argv)
 #ifdef _MSC_VER
 		processFilesRecursive(searchPath, useUvMesh);
 #else
-		useUvMesh = useUvMesh;
+		(void)useUvMesh;
 		logf("not implemented\n");
 #endif
 	} else {
